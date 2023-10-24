4b91574e8ac2bbf2372b87f8f1f2122e9aa767df
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-10-24 12:55:50 +0200

    Add list_to_data_frame_with_integer_keys()

--- R/utils.R
+++ R/utils.R
@@ -42,6 +42,41 @@ helpers_index <- function(x, values)
   which.min(abs(x - values)) - 1L
 }
 
+# list_to_data_frame_with_integer_keys -----------------------------------------
+
+#' Convert List of Similar Flat Sublists to a Data Frame
+#'
+#' @param x list of similar flat lists, i.e. lists that have list elements with
+#'   the same names and list elements that all have length one
+#' @param key_name name of column in the returned data frame that will contain
+#'   the integer values that are constructed from the element names in \code{x}
+#' @param key_pattern regular expression matching all element names in \code{x}.
+#'   The expression must contain one pair of parentheses enclosing the part that
+#'   can be converted to integer, e.g. \code{"element_([0-9]+)"}
+#' @return data frame with keys in a column named according to \code{key_name}
+#'   and value columns according to the list elements in the sublists of
+#'   \code{x}
+#' @export
+#' @examples
+#' list_to_data_frame_with_integer_keys(
+#'   x = list(
+#'     element_1 = list(a = 100, b = 10),
+#'     element_2 = list(a = 200, b = 20)
+#'   ),
+#'   key_name = "element",
+#'   key_pattern = "element_([0-9]+)"
+#' )
+list_to_data_frame_with_integer_keys <- function(x, key_name, key_pattern)
+{
+  stopifnot(is.list(x), all(grepl(key_pattern, names(x))))
+
+  result <- kwb.utils::rbindAll(lapply(x, as.data.frame), key_name)
+
+  result[[key_name]] <- as.integer(gsub(key_pattern, "\\1", result[[key_name]]))
+
+  result
+}
+
 # range_to_seq -----------------------------------------------------------------
 
 #' Sequence of Values Between the Range of Values in a Given Vector
