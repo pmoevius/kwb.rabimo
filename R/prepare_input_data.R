4f6889c87292a7f0db0a84afcf49c3b52cbe9442
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-11-18 12:20:13 +0100

    Extract INPUT_COLUMN_RENAMINGS
    and calculate_fractions()

--- R/prepare_input_data.R
+++ R/prepare_input_data.R
@@ -14,34 +14,18 @@
 prepare_input_data <- function(input_data)
 {
   # Rename columns from ABIMO 3.2 names to ABIMO 3.3 internal names
-  input <- rename_columns(input_data, list(
-    REGENJA = "precipitationYear",
-    REGENSO = "precipitationSummer",
-    NUTZUNG = "berlin_usage",
-    TYP = "berlin_type",
-    BEZIRK = "district",
-    FLGES = "mainArea",
-    STR_FLGES = "roadArea",
-    PROBAU = "mainPercentageBuiltSealed",
-    PROVGU = "mainPercentageUnbuiltSealed",
-    VGSTRASSE = "roadPercentageSealed",
-    KAN_BEB = "builtSealedPercentageConnected",
-    BELAG1 = "unbuiltSealedPercentageSurface1",
-    BELAG2 = "unbuiltSealedPercentageSurface2",
-    BELAG3 = "unbuiltSealedPercentageSurface3",
-    BELAG4 = "unbuiltSealedPercentageSurface4",
-    KAN_VGU = "unbuiltSealedPercentageConnected",
-    STR_BELAG1 = "roadSealedPercentageSurface1",
-    STR_BELAG2 = "roadSealedPercentageSurface2",
-    STR_BELAG3 = "roadSealedPercentageSurface3",
-    STR_BELAG4 = "roadSealedPercentageSurface4",
-    KAN_STR = "roadSealedPercentageConnected",
-    FLUR = "depthToWaterTable",
-    FELD_30 = "fieldCapacity_30",
-    FELD_150 = "fieldCapacity_150"
-  ))
+  input <- rename_columns(input_data, INPUT_COLUMN_RENAMINGS)
 
-  # Calculate the percentage of built and unbuild sealed areas. Add a small
+  # Create column accessor function
+  fetch <- create_accessor(input)
+
+  # Calculate total area
+  input[["totalArea"]] <- fetch("mainArea") + fetch("roadArea")
+
+  # Convert percentages to fractions
+  fractions <- calculate_fractions(input)
+
+  # Calculate the percentage of built and unbuilt sealed areas. Add a small
   # value to round .5 "up" not "down":
   # round(98.5) -> 98
   # round(98.5 + 1e-12) -> 99
@@ -51,43 +35,77 @@ prepare_input_data <- function(input_data)
   # vgb = (dbReader.getRecord(k, "PROVGU")).toFloat() / 100.0F; // Hofflaechen
   # ptrDA.VER = (int)round((vgd * 100) + (vgb * 100));
 
-  input[["mainPercentageSealed"]] <- as.integer(round(
-    select_columns(input, "mainPercentageBuiltSealed") +
-      select_columns(input, "mainPercentageUnbuiltSealed") +
-      1e-12
-  ))
+  fractions[["mainFractionSealed"]] <- as.integer(round(
+    1e-12 +
+      fetch("mainPercentageBuiltSealed") +
+      fetch("mainPercentageUnbuiltSealed")
+  )) / 100
+
+  # Get (usage, yield, irrigation) tuples based on Berlin-specific codes
+  usages <- get_usage_tuple(
+    usage = fetch("berlin_usage"),
+    type = fetch("berlin_type")
+  )
+
+  # Column-bind everything together
+  cbind(input, fractions, usages)
+}
+
+# INPUT_COLUMN_RENAMINGS -------------------------------------------------------
+INPUT_COLUMN_RENAMINGS <- list(
+  REGENJA = "precipitationYear",
+  REGENSO = "precipitationSummer",
+  NUTZUNG = "berlin_usage",
+  TYP = "berlin_type",
+  BEZIRK = "district",
+  FLGES = "mainArea",
+  STR_FLGES = "roadArea",
+  PROBAU = "mainPercentageBuiltSealed",
+  PROVGU = "mainPercentageUnbuiltSealed",
+  VGSTRASSE = "roadPercentageSealed",
+  KAN_BEB = "builtSealedPercentageConnected",
+  BELAG1 = "unbuiltSealedPercentageSurface1",
+  BELAG2 = "unbuiltSealedPercentageSurface2",
+  BELAG3 = "unbuiltSealedPercentageSurface3",
+  BELAG4 = "unbuiltSealedPercentageSurface4",
+  KAN_VGU = "unbuiltSealedPercentageConnected",
+  STR_BELAG1 = "roadSealedPercentageSurface1",
+  STR_BELAG2 = "roadSealedPercentageSurface2",
+  STR_BELAG3 = "roadSealedPercentageSurface3",
+  STR_BELAG4 = "roadSealedPercentageSurface4",
+  KAN_STR = "roadSealedPercentageConnected",
+  FLUR = "depthToWaterTable",
+  FELD_30 = "fieldCapacity_30",
+  FELD_150 = "fieldCapacity_150"
+)
+
+# calculate_fractions ----------------------------------------------------------
+calculate_fractions <- function(input)
+{
+  # Column accessor
+  fetch <- create_accessor(input)
 
   # Helper function to select column and divide by 100
-  by_100 <- function(x) select_columns(input, x) / 100
+  by_100 <- function(x) fetch(x) / 100
 
-  # Calculate additional columns (e.g. percentage to fraction)
-  main_area <- select_columns(input, "mainArea")
-  road_area <- select_columns(input, "roadArea")
-  total_area <-  main_area + road_area
+  total_area <- fetch("total_area")
 
-  input[["totalArea"]] <- total_area
-  input[["areaFractionMain"]] <- select_columns(input, "mainArea") / total_area
-  input[["areaFractionRoad"]] <- select_columns(input, "roadArea") / total_area
-
-  input[["mainFractionBuiltSealed"]] <- by_100("mainPercentageBuiltSealed")
-  input[["mainFractionUnbuiltSealed"]] <- by_100("mainPercentageUnbuiltSealed")
-  input[["mainFractionSealed"]] <- by_100("mainPercentageSealed")
-  input[["roadFractionRoadSealed"]] <- by_100("roadPercentageSealed")
-  input[["builtSealedFractionConnected"]] <- by_100("builtSealedPercentageConnected")
-  input[["unbuiltSealedFractionSurface1"]] <- by_100("unbuiltSealedPercentageSurface1")
-  input[["unbuiltSealedFractionSurface2"]] <- by_100("unbuiltSealedPercentageSurface2")
-  input[["unbuiltSealedFractionSurface3"]] <- by_100("unbuiltSealedPercentageSurface3")
-  input[["unbuiltSealedFractionSurface4"]] <- by_100("unbuiltSealedPercentageSurface4")
-  input[["unbuiltSealedFractionConnected"]] <- by_100("unbuiltSealedPercentageConnected")
-  input[["roadSealedFractionSurface1"]] <- by_100("roadSealedPercentageSurface1")
-  input[["roadSealedFractionSurface2"]] <- by_100("roadSealedPercentageSurface2")
-  input[["roadSealedFractionSurface3"]] <- by_100("roadSealedPercentageSurface3")
-  input[["roadSealedFractionSurface4"]] <- by_100("roadSealedPercentageSurface4")
-  input[["roadSealedFractionConnected"]] <- by_100("roadSealedPercentageConnected")
-
-  # Add the "usage tuple" as columns
-  cbind(input, get_usage_tuple(
-    usage = select_columns(input, "berlin_usage"),
-    type = select_columns(input, "berlin_type")
-  ))
+  data.frame(
+    areaFractionMain = fetch("mainArea") / total_area,
+    areaFractionRoad = fetch("roadArea") / total_area,
+    mainFractionBuiltSealed = by_100("mainPercentageBuiltSealed"),
+    mainFractionUnbuiltSealed = by_100("mainPercentageUnbuiltSealed"),
+    roadFractionRoadSealed = by_100("roadPercentageSealed"),
+    builtSealedFractionConnected = by_100("builtSealedPercentageConnected"),
+    unbuiltSealedFractionConnected = by_100("unbuiltSealedPercentageConnected"),
+    unbuiltSealedFractionSurface1 = by_100("unbuiltSealedPercentageSurface1"),
+    unbuiltSealedFractionSurface2 = by_100("unbuiltSealedPercentageSurface2"),
+    unbuiltSealedFractionSurface3 = by_100("unbuiltSealedPercentageSurface3"),
+    unbuiltSealedFractionSurface4 = by_100("unbuiltSealedPercentageSurface4"),
+    roadSealedFractionConnected = by_100("roadSealedPercentageConnected"),
+    roadSealedFractionSurface1 = by_100("roadSealedPercentageSurface1"),
+    roadSealedFractionSurface2 = by_100("roadSealedPercentageSurface2"),
+    roadSealedFractionSurface3 = by_100("roadSealedPercentageSurface3"),
+    roadSealedFractionSurface4 = by_100("roadSealedPercentageSurface4")
+  )
 }
