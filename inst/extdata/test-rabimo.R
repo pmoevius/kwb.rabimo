f04c99f7ceae67c997e64176af10fae755437b28
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-02-16 13:02:55 +0100

    Clean test-rabimo.R
    - Add short intro header
    - Use path dictionary and accessor get_path()
    - Use helper functions
      - table_with_na()
      - has_gw_dist()

--- inst/extdata/test-rabimo.R
+++ inst/extdata/test-rabimo.R
@@ -1,39 +1,80 @@
+#
+# Test kwb.rabimo
+#
+# - Source the whole script first to load the functions defined below!
+#
 
-# parameter for testing, to delete later
-if(FALSE)
+if (FALSE)
 {
-  kwb.utils::assignPackageObjects("kwb.rabimo");simulate_abimo = TRUE
+  #kwb.utils::assignPackageObjects("kwb.rabimo");simulate_abimo = TRUE
 
-  #paths
-  path_amarex_ap4 <- "Y:/SUW_Department/Projects/AMAREX/Work-packages/AP_4"
-  path_data_2020 <- file.path(
-    path_amarex_ap4,
-    "ABIMO_Daten/ISU5_2020_datengrundlage/isu5_2020_berlin/cleaned"
-  )
+  # Define a path "dictionary"
+  get_path <- kwb.utils::createAccessor(kwb.utils::resolve(list(
+    amarex_ap4 = "//medusaY:/SUW_Department/Projects/AMAREX/Work-packages/AP_4",
+    isu5_2020 = "<amarex_ap4>/ABIMO_Daten/ISU5_2020_datengrundlage",
+    data_2020 = "<isu5_2020>/isu5_2020_berlin/cleaned",
+    berlin_2020 = "<data_2020>/isu5_2020_abimo_cleaned.dbf",
+    ndvi = "Y:/Z-Exchange/Philipp/Amarex/NDVI R/combined_data_NDVI.dbf"
+  )))
 
-  file_berlin_2020 <- file.path(path_data_2020, "isu5_2020_abimo_cleaned.dbf")
+  berlin_2020_data <- foreign::read.dbf(get_path("berlin_2020"))
 
-  berlin_2020_data <- foreign::read.dbf(file_berlin_2020) #PROBLEM: HANDLE NAs!!
+  # PROBLEM: HANDLE NAs!!
+
+  # Clean column "STR_FLGES"
+  table_with_na(berlin_2020_data$STR_FLGES)
   berlin_2020_data$STR_FLGES <- 0
-  #berlin_2019_data <- kwb.abimo::abimo_input_2019
-  config <- abimo_config_to_config(kwb.abimo::read_config())
-  input_backup <- prepare_input_data(berlin_2020_data, config)
 
-  ndvi_matrix <- foreign::read.dbf("Y:/Z-Exchange/Philipp/Amarex/NDVI R/combined_data_NDVI.dbf")
-  input_ndvi <- dplyr::left_join(input_backup,
-                                 kwb.utils::selectColumns(ndvi_matrix,
-                                                          c("code", "ndvi_value")
-                                 ),
-                                 by = "code"
+  #berlin_2019_data <- kwb.abimo::abimo_input_2019
+
+  # Convert input data to the new format
+  input_backup <- kwb.rabimo::prepare_input_data(
+    input_data = berlin_2020_data,
+    config = kwb.rabimo::abimo_config_to_config(kwb.abimo::read_config())
   )
+
+  # Read NDVI data
+  ndvi_matrix <- foreign::read.dbf(get_path("ndvi"))
+
+  # Merge the NDVI value to the input data (by the codes of the blocks)
+  input_ndvi <- dplyr::left_join(
+    input_backup,
+    kwb.utils::selectColumns(ndvi_matrix, c("code", "ndvi_value")),
+    by = "code"
+  )
+
+  # Do we have an NDVI value for each block?
+  table_with_na(input_ndvi$ndvi_value)
+
+  #  30    35    40    45    50  <NA>
+  #   1  1583 24192 29384  3371     0
+
+  # Save the original vegetation class (as assigned by hard-coded rules in
+  # original ABIMO source code)
   old_veg_class <- input_ndvi$veg_class
+
+  # Use the NDVI value as vegetation class
   input_ndvi[["veg_class"]] <- input_ndvi[["ndvi_value"]]
+
+  # Remove the NDVI column
   input_ndvi[["ndvi_value"]] <- NULL
 
   # Remove rows where groundwater distance is NA
-  input <- input_ndvi[kwb.utils::matchesCriteria(input_ndvi, "!is.na(gw_dist)"), ]
+  input <- input_ndvi[has_gw_dist(input_ndvi), ]
 
   # use for old vegetation classes
-  input <- input_backup[kwb.utils::matchesCriteria(input_backup, "!is.na(gw_dist)"), ]
+  input <- input_backup[has_gw_dist(input_backup), ]
   input <- head(input)
+}
+
+# Define function: table_with_na() ---------------------------------------------
+table_with_na <- function(x)
+{
+  table(x, useNA = "always")
+}
+
+# Define function: has_gw_dist() -----------------------------------------------
+has_gw_dist <- function(data)
+{
+  kwb.utils::matchesCriteria(data, "!is.na(gw_dist)")
 }
