8efc581e6f41fa9cb2b08427d965d6a45ead5b32
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-02-23 10:36:28 +0100

    Rename where_fun to check, where_text to text

--- R/utils.R
+++ R/utils.R
@@ -326,7 +326,7 @@ seq_along_rows <- function(data)
 }
 
 # set_columns_to_zero ----------------------------------------------------------
-set_columns_to_zero <- function(data, columns, where_fun, where_text)
+set_columns_to_zero <- function(data, columns, check, text)
 {
   stopifnot(is.data.frame(data))
 
@@ -336,7 +336,7 @@ set_columns_to_zero <- function(data, columns, where_fun, where_text)
 
     stopifnot(is.numeric(x))
 
-    meets_condition <- where_fun(x)
+    meets_condition <- check(x)
 
     if (!any(meets_condition)) {
       next
@@ -347,7 +347,7 @@ set_columns_to_zero <- function(data, columns, where_fun, where_text)
         "Setting %d value(s) in \"%s\" to 0 where %s",
         sum(meets_condition),
         column,
-        where_text
+        text
       ),
       expr = {
         x[meets_condition] <- 0
@@ -367,8 +367,8 @@ set_columns_to_zero_where_almost_zero <- function(
   set_columns_to_zero(
     data = data,
     columns = columns,
-    where_fun = function(x) abs(x) < threshold,
-    where_text = paste("value <", threshold)
+    check = function(x) abs(x) < threshold,
+    text = paste("value <", threshold)
   )
 }
 
@@ -379,8 +379,8 @@ set_columns_to_zero_where_na <- function(data, columns)
   set_columns_to_zero(
     data = data,
     columns = columns,
-    where_fun = is.na,
-    where_text = "value is NA"
+    check = is.na,
+    text = "value is NA"
   )
 }
 
