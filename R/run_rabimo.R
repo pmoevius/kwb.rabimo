cde7c1c7b56454cd66c0bf7e008ee3495920e957
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-03-18 22:02:02 +0100

    Put into one line what fits into one line

--- R/run_rabimo.R
+++ R/run_rabimo.R
@@ -264,17 +264,10 @@ run_rabimo <- function(data, config, simulate_abimo = TRUE)
   )
 }
 
-# get_climate ------------------------------------------------------------------
-# provides climate relevant input data
-
+# get_climate: provides climate relevant input data ----------------------------
 get_climate <- function(input)
 {
-  climate <- select_columns(input, c(
-    "prec_yr",
-    "prec_s",
-    "epot_yr",
-    "epot_s"
-  ))
+  climate <- select_columns(input, c("prec_yr", "prec_s", "epot_yr", "epot_s"))
 
   climate[["x_ratio"]] <- climate[["prec_yr"]] / climate[["epot_yr"]]
 
