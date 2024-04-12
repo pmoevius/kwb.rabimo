18a34e5ef8e2affac5a43be5ae1f8842093b3ade
f-delpu <francesco.del-punta@kompetenz-wasser.de>
2024-04-12 17:22:12 +0200

    Reorganize script and compare different areas

--- inst/extdata/calibration-stormwater.R
+++ inst/extdata/calibration-stormwater.R
@@ -1,122 +1,229 @@
 # Calibration script to set stormwater-management parameters
 # based on the WABILA equations
-kwb.utils::assignPackageObjects("kwb.rabimo")
+kwb.utils::assignPackageObjects("kwb.rabimo");`%>%` <- magrittr::`%>%`
 
 # import config object
 config <- kwb.rabimo::rabimo_inputs_2020$config
 
-# modify config to match wabila simulation
-config$bagrov_values["green_roof"] <- 0.5
-config$swale["swale_evaporation_factor"] <- 0.0
+# Method:
+#   - generate test areas
+#   - model them in Wabila
+#   - model them in Rabimo, compare results and assess differences
+
+# main
+if (FALSE) {
+
+  # CASE 1) simple roof area, 100% connected to the sewer ~~~~~~~~~~~~~~~~~~~~~~
+  simple_roof_area <- generate_rabimo_area(
+    code = "only_roof", roof = 1, pvd = 0
+  )
+
+  wabila_roof <- calculate_wabila_roof(
+    area = simple_roof_area, retention_height = 0.5
+  )
+
+  factor_simple_roof <- get_area_balance(area = simple_roof_area,
+                                         config = config,
+                                         wabila_results = wabila_roof)
+  factor_simple_roof
+
+  # CASE 2) green roof area: the whole roof is green ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  green_roof_area <- generate_rabimo_area(
+    code = "only_green_roof", roof = 1, green_roof = 1, pvd = 0)
+
+  wabila_green_roof <- calculate_wabila_green_roof(
+    area = green_roof_area, height = 40, kf = 70, w_diff = 0.5)
 
 
-# generate test areas -------------------------------------------------------
-simple_roof_area <- generate_rabimo_area(code = "only_roof",
-                                         roof = 1, pvd = 0)
+  factor_green_roof <- get_area_balance(area = green_roof_area,
+                                        config = config,
+                                        wabila_results = wabila_green_roof)
 
-green_roof_area <- generate_rabimo_area(code = "only_green_roof",
-                                        roof = 1, green_roof = 1, pvd = 0)
+  # case 3) simple paved area, no roof, no unsealed surface ~~~~~~~~~~~~~~~~~~~~
+  paved_area <- generate_rabimo_area(
+    code = "all_paved", roof = 0, pvd = 1, swg_pvd = 0.38,
+    srf3_pvd = 0.1, srf4_pvd = 0.2)
 
-paved_area <- generate_rabimo_area(code = "all_paved", roof = 0, pvd = 1,
-                                   swg_pvd = 0.38, srf3_pvd = 0.1, srf4_pvd = 0.2)
+  wabila_paved <- calculate_wabila_paved(
+    area = paved_area, joint_perc = 8, retention = 1, kf = 36, w_diff = 0.15)
 
-area_to_swale <- generate_rabimo_area(code = "roof_to_swale",
-                                      roof = 1, pvd = 0, to_swale = 1)
-# problem: the swale must be outside the area since roof = 1 (whole area)
-# this can not be modeled in WABILA
+  factors_paved_area <- get_area_balance(area = paved_area,
+                                         config = config,
+                                         wabila_results = wabila_paved)
 
-# no runoff
-only_garden_area <- generate_rabimo_area(code ="garden", roof = 0, pvd = 0)
+  # CASE 4) only garden area, non constructed ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# area with 50% roof and 50% garden. The ro from the roof goes into the sewer.
-mixed_roof_garden_area <- generate_rabimo_area(code = "roof_and_garden",
-                                               roof = 0.5, pvd = 0)
+  only_garden_area <- generate_rabimo_area(code ="garden", roof = 0, pvd = 0,
+                                           veg_class = 20)
 
+  wabila_garden <- c(0.0, 0.4, 0.6)
 
-
-# model areas in WABILA --------------------------------------------------------
-
-# get climatic parameters
-#
-P <- simple_roof_area$prec_yr
-ET_p <- simple_roof_area$epot_yr
-
-## "normal" roof equation ###
-Sp <- 0.25 # retention, in  mm
-
-a_roof <- 0.9115 + 0.00007063 * - 0.000007498 * ET_p - 0.2063 * log(Sp + 1)
-g_roof <- 0
-v_roof <- 1 - a_roof
-
-wabila_roof_results <- c(a_roof, g_roof, v_roof)
-
-## green-roof equation ###
-h <- 40 # depth, in cm
-k_f <- 70 # permeability of joints, in mm/h
-w <- 0.5 # difference between water holding capacity and wilting point, unitless
-
-a_green_roof = -2.182 + 0.4293 * log(P) - 0.0001092 * P +  236.1/ET_p +
-  0.0001142 * h + 0.0002297 * k_f + 0.01628 * log(w) - 0.1214 * log((w) * h)
-g_green_roof <- 0
-v_green_roof <- 1 - a_green_roof
-
-wabila_green_roof_results <- c(a_green_roof, g_green_roof, v_green_roof)
-
-## equation for semi-permeable surfaces (6% to 10% joints) ###
-FA = 8 # percentage of joints
-Sp <- 1 # retention, in  mm
-k_f <- 36 # permeability, in mm/h
-w <- 0.15 # difference between water holding capacity and wilting point, unitless
-
-a_pavement = 0.05912 * log(P) - 0.02749 * FA - 0.03671 * Sp - 0.30514 * (w) +
-  4.97687 / (4.7975 + k_f)
-g_pavement = 0.00004941 * P - 0.0002817 * ET_p + 0.02566 * FA - 0.03823 * Sp +
-  0.691 * exp(-6.465/k_f)
-v_pavement = 0.9012 - 0.1325 * log(P) + 0.00006661 * ET_p + 0.002302 * FA +
-  0.1489 * log(1 + Sp)
-
-wabila_paved_results <- c(a_pavement, g_pavement, v_pavement)
-
-## equation for infiltration swale ###
-k_f = 14 # permeability, in mm/h
-BA = 42.323 * k_f^(-0.314) # percentage of swale area (what is it?)
-
-g_swale = 0.8608 + 0.02385 * log(P) - 0.00005331 * ET_p - 0.002827 * BA -
-  0.000002493 * k_f + 0.0009514 * log(k_f/BA)
-v_swale = 0.000008562 * ET_p + 2.611/(-64.35 + P) * BA ^ 0.9425 -
-  0.000001211 * k_f
-a_swale = 1 - g_swale - v_swale
-
-wabila_roof_to_swale_results <- c(a_swale, g_swale, v_swale)
-# ??? how to take into account cascading effects and dimension of swale???
-
-# compare result in R-ABIMO ----------------------------------------------------
-
-# express results in percentage and compare with WABILA
-factor_simple_roof <- get_area_balance(area = simple_roof_area,
-                                       config = config,
-                                       wabila_results = wabila_roof_results)
-
-factor_green_roof <- get_area_balance(area = green_roof_area,
-                                       config = config,
-                                       wabila_results = wabila_green_roof_results)
-
-factors_paved_area <- get_area_balance(area = paved_area,
-                                      config = config,
-                                      wabila_results = wabila_paved_results)
-
-factors_roof_to_swale <- get_area_balance(area = area_to_swale,
+  factors_only_garden <- get_area_balance(area = only_garden_area,
                                           config = config,
-                                          wabila_results = wabila_roof_to_swale_results)
+                                          wabila_results = wabila_garden)
+
+  # case 5) mixed roof/garden area ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # 50% roof and 50% garden. The runoff from the roof goes into the sewer.
+  mixed_roof_garden_area <- generate_rabimo_area(code = "roof_and_garden",
+                                                 roof = 0.5, pvd = 0,
+                                                 veg_class = 20)
+
+  roof_fraction <- mixed_roof_garden_area$roof
+  wabila_mix_roof_garden <- wabila_garden * roof_fraction +
+    wabila_roof * (1 - roof_fraction)
+
+  factors_mixed_garden <- get_area_balance(area = mixed_roof_garden_area,
+                                           config = config,
+                                           wabila_results = wabila_mix_roof_garden)
+
+  # case 6) area connected to an infiltration swale ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  area_to_swale <- generate_rabimo_area(code = "roof_to_swale",
+                                        roof = 0.5, pvd = 0, to_swale = 1,
+                                        veg_class = 20)
+  # problem: "free" area for the swale must be taken into account
+  # when comparing results with wabila simulation
+
+  # Wabila simulation:
+  # calculate first water-balance from roof area (how?)
+  discharge_into_swale <- mixed_roof_garden_area$prec_yr *
+    factors_mixed_garden["rabimo","runoff"]
+
+  wabila_swale <- calculate_wabila_swale(
+    area = area_to_swale, kf = 14)
+
+  roof_fraction <- area_to_swale$roof
+  swale_fraction <- attr(wabila_swale, "estimated swale area")
+  garden_fraction <- 1 - roof_fraction - swale_fraction
+
+  wabila_swale_roof_garden <- c(
+    a = swale_fraction * wabila_swale[1] + garden_fraction * wabila_garden[1],
+    g = roof_fraction * (wabila_roof[1] + wabila_roof[2]) + garden_fraction * wabila_garden[2] + swale_fraction * wabila_swale[2],
+    v = roof_fraction * wabila_roof[3] + swale_fraction * wabila_swale[3] + garden_fraction * wabila_garden[3]
+  )
 
 
-factors_only_garden <- get_area_balance(area = only_garden_area,
-                                        config = config)
+  # modify config to match wabila simulation
+  config$swale["swale_evaporation_factor"] <- 0.0
 
-factors_mixed_garden <- get_area_balance(area = mixed_roof_garden_area,
-                                         config = config)
+  factors_roof_to_swale <- get_area_balance(area = area_to_swale,
+                                            config = config,
+                                            wabila_results = wabila_swale_roof_garden)
+}
 
+# calculate_wabila_roof --------------------------------------------------------
+calculate_wabila_roof <- function(area, retention_height)
+{
+  # get climatic parameters from area
+  P <- area$prec_yr
+  ET_p <- area$epot_yr
 
+  # runoff component
+  a <- 0.9115 + 0.00007063 * P - 0.000007498 * ET_p - 0.2063 *
+    log(retention_height + 1)
+
+  # infiltration component
+  g <- 0
+
+  #evaporation component
+  v <- 1 - a
+
+  c(a, g, v)
+
+}
+
+# calculate_wabila_green_roof --------------------------------------------------
+calculate_wabila_green_roof <- function(area, height, kf, w_diff)
+{
+
+  # h: depth, in cm
+  # kf: permeability of soil, in mm/h
+  # w_diff: difference between water holding capacity and wilting point, unitless
+
+  # get climatic parameters from area
+  P <- area$prec_yr
+  ET_p <- area$epot_yr
+
+  # runoff component
+  a = -2.182 + 0.4293 * log(P) - 0.0001092 * P +  236.1/ET_p +
+    0.0001142 * height + 0.0002297 * kf + 0.01628 * log(w_diff) - 0.1214 *
+    log((w_diff) * height)
+
+  # infiltration component
+  g <- 0
+
+  #evaporation component
+  v <- 1 - a
+
+  c(a, g, v)
+}
+
+# calculate_wabila_paved -------------------------------------------------------
+calculate_wabila_paved <- function(area, joint_perc, retention, kf, w_diff){
+
+  # joint_perc: percentage of joints
+  # retention: retention_heigth, in mm
+  # kf: permeability of joints, in mm/h
+  # w_diff: difference between water holding capacity and wilting point, unitless
+
+  # get climatic parameters from area
+  P <- area$prec_yr
+  ET_p <- area$epot_yr
+
+  if (2 <= joint_perc & 5.5 >= joint_perc)
+  {
+    a = 0.800734 * log(P) - 0.0582828 * joint_perc - 0.0501693 *
+      retention - 0.385767 * (w_diff) + 8.7040284 / (11.9086896 + kf)
+    g = -0.2006 - 0.000253 * ET_p + 0.05615 * joint_perc - 0.0636 *
+      log(1 + retention) + 0.1596 * log(1 + kf) + 0.2778 * w_diff
+    v = 0.8529 - 0.1248 * log(P) + 0.00005057 * ET_p + 0.002372 *
+      joint_perc + 0.1583 * log(1 + retention)
+
+  } else if (5.5 < joint_perc & 10 >= joint_perc)
+  {
+    a = 0.05912 * log(P) - 0.02749 * joint_perc - 0.03671 *
+      retention - 0.30514 * (w_diff) + 4.97687 / (4.7975 + kf)
+    g = 0.00004941 * P - 0.0002817 * ET_p + 0.02566 *
+      joint_perc - 0.03823 * retention + 0.691 * exp(-6.465 / kf)
+    v = 0.9012 - 0.1325 * log(P) + 0.00006661 * ET_p + 0.002302 * joint_perc +
+      0.1489 * log(1 + retention)
+  }
+
+  c(a, g, v)
+}
+
+# calculate_wabila_swale -------------------------------------------------------
+calculate_wabila_swale <- function(input = NULL, area, kf, BA = NULL)
+{
+
+  # kf: permeability, in mm/h
+  # input: the discharge to the swale in mm
+  # BA: percentage of swale area (what is it?)
+
+  # get climatic parameters from area
+  P <- area$prec_yr
+  ET_p <- area$epot_yr
+
+  if (is.null(input)){
+    input <- P
+  }
+
+  if (is.null(BA)){
+    BA = 42.323 * kf^(-0.314)
+  }
+
+  g = 0.8608 + 0.02385 * log(input) - 0.00005331 * ET_p - 0.002827 * BA -
+    0.000002493 * kf + 0.0009514 * log(kf/BA)
+  v = 0.000008562 * ET_p + 2.611/(-64.35 + input) * BA ^ 0.9425 -
+    0.000001211 * kf
+  a = 1 - g - v
+
+  result <- c(a, g, v)
+  attr(result, "estimated swale area") <- BA/100
+
+  return(result)
+
+}
 
 # get_area_balance -------------------------------------------------------------
 get_area_balance <- function(area, config, rabimo_results = NULL,
