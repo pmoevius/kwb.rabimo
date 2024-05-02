1c6fc8feef7442f63eac2ce78389a993ba14f926
f-delpu <francesco.del-punta@kompetenz-wasser.de>
2024-05-02 17:06:56 +0200

    Add documentation for read_column_info()

--- R/prepare_input_data.R
+++ R/prepare_input_data.R
@@ -161,6 +161,12 @@ get_column_renamings <- function()
 }
 
 # read_column_info -------------------------------------------------------------
+
+#' Provide Meta Information About Input Columns
+#'
+#' @returns data frame with columns "rabimo_berlin", "abimo_berlin", "by_100",
+#'   "meaning", "unit", "type", "data_type", "default"
+#' @export
 read_column_info <- function()
 {
   "extdata/column-names.csv" %>%
