74de6c924bc77b997733bf0ea6315096fe695fda
f-delpu <francesco.del-punta@kompetenz-wasser.de>
2024-05-06 13:24:23 +0200

    Write function to make dataset "natural"

--- inst/extdata/natural-scenario.R
+++ inst/extdata/natural-scenario.R
@@ -11,18 +11,11 @@ results <- kwb.utils:::get_cached("rabimo_results_2020")
 `%>%` <- magrittr::`%>%`
 
 # modify inputs to simulate the natural state
-nat_input_data_1 <- inputs$data %>%
-  dplyr::mutate(roof = 0, pvd = 0, pvd_rd = 0, sealed = 0, irrigation = 0)
+nat_input_data_1 <- data_to_natural(inputs$data)
 
-nat_input_data_2 <- inputs$data %>%
-  dplyr::mutate(
-    roof = 0, pvd = 0, pvd_rd = 0, sealed = 0, irrigation = 0, land_type = "forested"
-  )
+nat_input_data_2 <- data_to_natural(inputs$data, type = "forested")
 
-nat_input_data_3 <- inputs$data %>%
-  dplyr::mutate(
-    roof = 0, pvd = 0, pvd_rd = 0, sealed = 0, irrigation = 0, land_type = "horticultural"
-  )
+nat_input_data_3 <- data_to_natural(inputs$data, type = "horticultural")
 
 # calculate water balance for natural scenario(S)
 nat_result_1 <- run_rabimo(data = nat_input_data_1, config = inputs$config,
@@ -33,7 +26,6 @@ nat_result_2 <- run_rabimo(data = nat_input_data_2, config = inputs$config,
 
 nat_result_3 <- run_rabimo(data = nat_input_data_3, config = inputs$config,
                            check = FALSE, intermediates = TRUE, simulate_abimo = FALSE)
-
 
 # compare natural results
 nat_result_1$source <- "undeveloped"
@@ -50,18 +42,76 @@ combined_results_long <- combined_results %>%
   dplyr::filter(variable != "total_surface_runoff")
 
 land_type_colors <- c("undeveloped" = "coral2",
-                      "forested" = "darkseagreen",
-                      "horticultural" = "cadetblue3")
+                      "forested" = "darkolivegreen3",
+                      "horticultural" = "cornflowerblue")
 
-nat_res_plot <-
+# all berlin areas
+nat_res_plot_all <-
   ggplot2::ggplot(data = combined_results_long,
                   mapping =  ggplot2::aes(x = value, fill = source)) +
-  ggplot2::geom_histogram(binwidth = 1, position = "dodge") +
+  ggplot2::geom_histogram(binwidth = 5, position = "dodge") +
   ggplot2::facet_wrap(~ variable, scale = "free") +
   ggplot2::labs(title = "Comparison of Different Natural Water Balance Scenarios") +
   ggplot2::scale_fill_manual(values = land_type_colors) +
   ggplot2::theme_minimal()
 
+nat_res_plot_all
+
+# exclude water bodies and forests
+exclude_patterns <- c("water", "forest")
+
+filtered_input_codes <- inputs$data %>%
+  dplyr::mutate(block_type = tolower(block_type)) %>%
+  dplyr::filter(!grepl(paste(exclude_patterns, collapse = "|"), block_type)) %>%
+  dplyr::select(code) %>%
+  dplyr::pull()
+
+filtered_results_long <- combined_results_long %>%
+  dplyr::filter(code %in% filtered_input_codes)
+
+nat_res_plot_no_water_no_forest <-
+  ggplot2::ggplot(data = filtered_results_long,
+                  mapping =  ggplot2::aes(x = value, fill = source)) +
+  ggplot2::geom_histogram(binwidth = 5, position = "dodge") +
+  ggplot2::facet_wrap(~ variable, scale = "free") +
+  ggplot2::labs(title = "Comparison of Different Natural Water Balance Scenarios (Excluding Water Bodies and Forests)") +
+  ggplot2::scale_fill_manual(values = land_type_colors) +
+  ggplot2::theme_minimal()
+
+nat_res_plot_no_water_no_forest
+
+# data_to_natural --------------------------------------------------------------
+data_to_natural <- function(data, type = "undeveloped")
+{
+  # check if data has r-abimo format
+  required_columns <- read_column_info() %>%
+    dplyr::filter(type == "required") %>%
+    dplyr::select(rabimo_berlin) %>%
+    dplyr::pull()
+
+  stopifnot(all(required_columns %in% names(data)))
+
+  # define patterns for column names rlated to urbanisation
+  patterns <- c("pv", "swg", "roof", "sealed")
+  urban_columns <- grep(paste(patterns, collapse = "|"), names(data), value = TRUE)
+
+  # non urbanized state: no building, no pavements
+  nat_data <- data %>%
+    dplyr::mutate(dplyr::across(dplyr::all_of(urban_columns), ~ 0))
+
+  if(type == "undeveloped"){
+    return(nat_data)
+  } else if (type == "forested"){
+    nat_data[["land_type"]] <- "forested"
+  } else if (type == "horticultural"){
+    nat_data[["land_type"]] <- "horticultural"
+  } else {
+    stop("please provide a known natural scenario type: undeveloped, horticultural or forested")
+  }
+
+  return(nat_data)
+
+}
 
 # calculate_delta_W ------------------------------------------------------------
 # natural <- nat_result_2[,-ncol(nat_result_1)]
