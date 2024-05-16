5255bc4ef10073b6b54e2103f60c27098661733c
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-05-16 15:44:03 +0200

    Remove depencency on dplyr, use create_accessor()
    and rename "arguments" to "key_value_pairs"

--- R/generate_rabimo_area.R
+++ R/generate_rabimo_area.R
@@ -7,30 +7,37 @@
 #' @param \dots key = value pairs overriding the default column values
 #' @param column_info data frame as returned by \code{\link{read_column_info}}
 #' @export
-generate_rabimo_area <- function(code, ..., column_info = read_column_info()) {
+generate_rabimo_area <- function(code, ..., column_info = read_column_info())
+{
+  #kwb.utils::assignPackageObjects("kwb.rabimo");column_info=read_column_info();`%>%`<-magrittr::`%>%`
 
-  dictionary <- column_info %>%
-    dplyr::filter(.data[["type"]] != "berlin-specific") %>%
+  # Remove rows that describe Berlin-specific columns
+  types <- select_columns(column_info, "type")
+
+  dictionary <- column_info[types != "berlin-specific", ] %>%
     select_columns(c("rabimo_berlin", "default", "data_type"))
 
-  arguments <- dictionary$default %>%
+  # Create a list of key-value pairs
+  key_value_pairs <- dictionary$default %>%
     as.list() %>%
     stats::setNames(dictionary$rabimo_berlin)
 
+  # Convert numeric columns to numeric
   is_numeric <- dictionary$data_type == "numeric"
-  arguments[is_numeric] <- lapply(arguments[is_numeric], as.numeric)
+  key_value_pairs[is_numeric] <- lapply(key_value_pairs[is_numeric], as.numeric)
 
-  result <- kwb.utils::callWith(data.frame, arguments, ...)
+  # Convert integer columns to integer
+  is_integer <- dictionary$data_type == "integer"
+  key_value_pairs[is_integer] <- lapply(key_value_pairs[is_integer], as.integer)
 
+  # Compose a one-row data frame from the key-value pairs
+  result <- kwb.utils::callWith(data.frame, key_value_pairs, ...)
+
+  # Add columns "code", "main_fraction", "sealed"
+  fetch <- create_accessor(result)
   result["code"] <- code
-
-  area_main <- result[["area_main"]]
-  total_area <- result[["total_area"]]
-  result["main_fraction"] <- round(area_main/total_area , 2)
-
-  roof <- result[["roof"]]
-  paved <- result[["pvd"]]
-  result["sealed"] <- round(roof + paved, 2)
+  result["main_fraction"] <- round(fetch("area_main")/fetch("total_area") , 2L)
+  result["sealed"] <- round(fetch("roof") + fetch("pvd"), 2L)
 
   result
 }
